aaf917e64e5c5009adf232d8143a06ae2ccc5b13
ayushbajracharya02 <ayushbajracharya02@gmail.com>
2024-05-08 13:42:44 +0545

    inital-commit

---
+++ backend/app.ts
@@ -0,0 +1,30 @@
+const express = require("express");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const corsOptions = {
+	origin: ["http://localhost:5173"],
+	credentials: true,
+};
+const app = express();
+app.use(cors(corsOptions));
+const server = createServer(app);
+const io = new Server(server, {
+	cors: corsOptions,
+});
+
+app.get("/", (req: any, res: any) => {
+	res.send("<h1>Hello world</h1>");
+});
+
+io.on("connection", (socket: any) => {
+	console.log(`a user connected at ${socket.id}\n`);
+	socket.on("send-message", (message: any) => {
+		socket.broadcast.emit("receive-message", message);
+	});
+});
+
+server.listen(3000, "127.0.0.1", () => {
+	console.log("server running at http://127.0.0.1:3000");
+});
---
+++ frontend/src/main.ts
@@ -0,0 +1,31 @@
+import { io } from "socket.io-client";
+import "bootstrap/scss/bootstrap.scss";
+import "./scss/main.scss";
+
+const socket = io("http://127.0.0.1:3000");
+
+document.querySelector("button")?.addEventListener("click", (e) => {
+	const messageBox: HTMLTextAreaElement = document.querySelector(
+		"#messageBox"
+	) as HTMLTextAreaElement;
+	if (!messageBox) {
+		return;
+	}
+	const message: string = messageBox.value;
+	displayMessage(message, "sent");
+	socket.emit("send-message", message);
+});
+
+socket.on("receive-message", (message) => {
+	displayMessage(message, "received");
+});
+
+function displayMessage(message: string, messageType: "sent" | "received") {
+	const formattedMessage = message.split("\n").join("<br/>");
+	const messageBlock = `<div class="col-12">
+			<div class="message-wrapper ${messageType}">
+				<span class="message ${messageType}">${formattedMessage}</span>
+			</div>
+	</div>`;
+	document.querySelector(".chat-app-messages")?.insertAdjacentHTML("beforeend", messageBlock);
+}
